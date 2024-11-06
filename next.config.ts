5d254396187e52a4efb8bd567cb0e05d1109b579
Brice Braquin <teambbocho@gmail.com>
2024-11-06 14:51:15 +0100

    feat: :ambulance: added netlify config for deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
-		domains: ['i.ytimg.com'],
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'i.ytimg.com',
+				port: '',
+				pathname: '/**',
+			},
+		],
 	},
 	reactStrictMode: false,
 };
