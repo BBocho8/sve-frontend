a6ae87987bcf46dede38f7f5ced26104d41c5262
Brice Braquin <teambbocho@gmail.com>
2024-11-06 14:59:07 +0100

    fix: changed next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-	/* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
 	images: {
 		remotePatterns: [
 			{
@@ -15,4 +13,4 @@ const nextConfig: NextConfig = {
 	reactStrictMode: false,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
