c501052068578157d1c081b1772acd48428db4be
Brice Braquin <teambbocho@gmail.com>
2024-11-13 17:01:37 +0100

    feat: :sparkles: implemented pagination

--- src/components/replay/Replay.tsx
+++ src/components/replay/Replay.tsx
@@ -1,17 +1,46 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import type { Video } from '@/types/Video';
+import { Pagination } from '@mui/material';
 import GamesContainer from './GamesContainer';
 
 type ReplayProps = {
 	data: Video[];
 };
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#15A34A',
+		},
+	},
+});
+
 const Replay = ({ data }: ReplayProps) => {
 	const [isCompetition, setIsCompetition] = useState('all');
-
 	const competitions = ['Bezirksliga', 'Kreisfreundschaftsspiele', 'Rheinlandpokal'];
+	const rowPerPage = 10;
+	const [filteredGames, setFilteredGames] = useState(data.slice(0, rowPerPage));
+	const [pages, setPages] = useState(Math.ceil(filteredGames.length / rowPerPage));
+	const [currentPage, setCurrentPage] = useState(1);
+
+	const handlePageChange = (value: number) => {
+		setCurrentPage(value);
+	};
+
+	useEffect(() => {
+		const filteredGamesEffect = data.filter(game => {
+			if (isCompetition === 'all') return true;
+			return game.competition === isCompetition;
+		});
+
+		setFilteredGames(filteredGamesEffect);
+		setPages(Math.ceil(filteredGamesEffect.length / rowPerPage));
+		setCurrentPage(1);
+	}, [isCompetition, data]);
 
 	return (
 		<section>
@@ -38,66 +67,47 @@ const Replay = ({ data }: ReplayProps) => {
 			</div>
 
 			<div className='grid items-center justify-center grid-cols-1 md:px-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-x-2'>
-				{isCompetition === 'all'
-					? data.map(game => {
-							const {
-								id,
-								isVideoAvailable,
+				{filteredGames.slice((currentPage - 1) * rowPerPage, currentPage * rowPerPage).map(game => {
+					const {
+						id,
+						isVideoAvailable,
 
-								competition,
-								homeTeam,
-								awayTeam,
+						competition,
+						homeTeam,
+						awayTeam,
 
-								date,
-								firstHalf1,
+						date,
+						firstHalf1,
 
-								fullGame,
-							} = game;
-							return (
-								<GamesContainer
-									key={game.id}
-									id={id}
-									isVideoAvailable={isVideoAvailable}
-									competition={competition}
-									homeTeam={homeTeam}
-									awayTeam={awayTeam}
-									date={date}
-									firstHalf1={firstHalf1}
-									fullGame={fullGame}
-								/>
-							);
-						})
-					: data
-							.filter(game => game.competition === isCompetition)
-							.map(game => {
-								const {
-									id,
-									isVideoAvailable,
-
-									competition,
-									homeTeam,
-									awayTeam,
-
-									date,
-									firstHalf1,
-
-									fullGame,
-								} = game;
-								return (
-									<GamesContainer
-										key={game.id}
-										id={id}
-										isVideoAvailable={isVideoAvailable}
-										competition={competition}
-										homeTeam={homeTeam}
-										awayTeam={awayTeam}
-										date={date}
-										firstHalf1={firstHalf1}
-										fullGame={fullGame}
-									/>
-								);
-							})}
+						fullGame,
+					} = game;
+					return (
+						<GamesContainer
+							key={game.id}
+							id={id}
+							isVideoAvailable={isVideoAvailable}
+							competition={competition}
+							homeTeam={homeTeam}
+							awayTeam={awayTeam}
+							date={date}
+							firstHalf1={firstHalf1}
+							fullGame={fullGame}
+						/>
+					);
+				})}
 			</div>
+			<ThemeProvider theme={theme}>
+				<Pagination
+					sx={{
+						display: 'flex',
+						justifyContent: 'center',
+					}}
+					count={pages}
+					color='primary'
+					page={currentPage}
+					onChange={(e, value) => handlePageChange(value)}
+				/>
+			</ThemeProvider>
 		</section>
 	);
 };
