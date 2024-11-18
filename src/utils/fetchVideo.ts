95224587f965de252aa3f9db00cb456863732702
Brice Braquin <teambbocho@gmail.com>
2024-11-18 17:19:21 +0100

    fix: added try catch in fetch video function

--- src/utils/fetchVideo.ts
+++ src/utils/fetchVideo.ts
@@ -1,9 +1,14 @@
 import type { VideoApiResponse } from '@/types/Video';
 
 export async function fetchVideos(domainUrl?: string) {
-	const response = await fetch(`${process.env.DOMAIN_URL || domainUrl}/api/videos`, { cache: 'no-cache' });
-	if (!response.ok) throw new Error('Failed to fetch videos games');
-	const data: VideoApiResponse = await response.json();
+	try {
+		const response = await fetch(`${process.env.DOMAIN_URL || domainUrl}/api/videos`, { cache: 'no-cache' });
+		if (!response.ok) throw new Error('Failed to fetch videos games');
+		const data: VideoApiResponse = await response.json();
 
-	return data.data;
+		return data.data;
+	} catch (error) {
+		console.log(error);
+		return [];
+	}
 }
