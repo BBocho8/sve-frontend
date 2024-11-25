5645cf646a627eaa23158efbfdd672644da7b78a
Brice Braquin <teambbocho@gmail.com>
2024-11-25 11:58:12 +0100

    fix: :bug: fixed issue with youtube api route

--- src/app/api/youtube/route.ts
+++ src/app/api/youtube/route.ts
@@ -1,51 +1,30 @@
-import ytdl from '@distube/ytdl-core';
-import { NextResponse } from 'next/server';
-import { createWriteStream, promises as fs } from 'node:fs';
-import { pipeline } from 'node:stream';
-import { promisify } from 'node:util';
+// import ytdl from '@distube/ytdl-core';
+// import { NextResponse } from 'next/server';
+// import { createWriteStream, promises as fs } from 'node:fs';
+// import { pipeline } from 'node:stream';
+// import { promisify } from 'node:util';
+// // }
+// const streamPipeline = promisify(pipeline);
 
 // export async function GET() {
-// 	// const data = await ytdl.getBasicInfo('https://youtu.be/F6sThOJiUNY?si=mGrpZtSJpdfJVAz3');
+// 	// const videoUrl = 'https://youtu.be/F6sThOJiUNY?si=mGrpZtSJpdfJVAz3';
+// 	// const videoStream = ytdl(videoUrl);
 
-// 	// const info = await ytdl.getInfo('https://youtu.be/F6sThOJiUNY?si=mGrpZtSJpdfJVAz3');
-// 	// const dl = ytdl('https://youtu.be/F6sThOJiUNY?si=mGrpZtSJpdfJVAz3').pipe(createWriteStream('video.mp4'));
+// 	// const tempFilePath = '/tmp/video.mp4';
+// 	// const writeStream = createWriteStream(tempFilePath);
 
-// 	// const test = await ytdl.getInfo('https://youtu.be/F6sThOJiUNY?si=mGrpZtSJpdfJVAz3');
+// 	// await streamPipeline(videoStream, writeStream);
 
-// 	// const video = ytdl('https://youtu.be/F6sThOJiUNY?si=mGrpZtSJpdfJVAz3');
+// 	// const fileBuffer = await fs.readFile(tempFilePath);
+// 	// const blob = new Blob([fileBuffer]);
 
-// 	// video.on('progress', info => {
-// 	// 	console.log('Download progress');
-// 	// });
-// 	// video.on('end', info => {
-// 	// 	console.log('Download finish');
-
-// 	// });
-
-// 	// video.pipe(createWriteStream('video.mp4'));
-
-// 	// video.pipe()
-// 	return ytdl('https://youtu.be/F6sThOJiUNY?si=mGrpZtSJpdfJVAz3');
-
-// 	// return Response.json({ video: 'test' });
+// 	return new NextResponse(blob, {
+// 		headers: {
+// 			'Content-Type': 'video/mp4',
+// 		},
+// 	});
 // }
-const streamPipeline = promisify(pipeline);
 
 export async function GET() {
-	const videoUrl = 'https://youtu.be/F6sThOJiUNY?si=mGrpZtSJpdfJVAz3';
-	const videoStream = ytdl(videoUrl);
-
-	const tempFilePath = '/tmp/video.mp4';
-	const writeStream = createWriteStream(tempFilePath);
-
-	await streamPipeline(videoStream, writeStream);
-
-	const fileBuffer = await fs.readFile(tempFilePath);
-	const blob = new Blob([fileBuffer]);
-
-	return new NextResponse(blob, {
-		headers: {
-			'Content-Type': 'video/mp4',
-		},
-	});
+	return new Response(JSON.stringify({ success: true }), { status: 200 });
 }
